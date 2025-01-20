b94d536ae7051cce3d921db2019ccfd5e74152fb
Aravinth <aravinth@homes247.in>
2025-01-20 12:44:02 +0530

    baseurl change

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/hr-connect-live/';
+    public $baseURL = 'http://localhost/hrconnect/';
 
     /**
      * --------------------------------------------------------------------------
